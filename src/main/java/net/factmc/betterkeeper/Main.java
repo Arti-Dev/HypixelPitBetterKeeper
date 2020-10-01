8e20292a29451475f6de52773f2b9719d21826dd
Arti_Dev <boomcreeper296@gmail.com>
2020-10-01 11:54:07 -0400

    Remove API Key because I'm an idiot

--- src/main/java/net/factmc/betterkeeper/Main.java
+++ src/main/java/net/factmc/betterkeeper/Main.java
@@ -7,7 +7,7 @@ public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getLogger().info("Firing up BetterKeeper!");
-		getConfig().addDefault("hypixelapikey", "0dba213a-ea64-432d-b931-72266f3a9f73");
+		getConfig().addDefault("hypixelapikey", "");
 		getConfig().options().copyDefaults(true); //make config file
 		saveConfig();
 		getServer().getPluginManager().registerEvents(new Listeners(this), this);
